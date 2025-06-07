c75d9f0afa8afe500d3bcd89e1681efe77f86c30
Akshath <akshathghanti@gmail.com>
2025-06-07 13:26:01 +0530

    modified:   app.py

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ except FileNotFoundError as e:
     st.stop()
 
 st.set_page_config(page_title="SOAP Note Builder", layout="centered")
-st.title("🩺 Doctor's SOAP Note Builder (Lite)")
+st.title("Subjective.Objective.Assessment.Plan Note")
 
 # Subjective
 st.header("1. Subjective")
